ff56b7bce6be0b19a445f838e9835db238d61b39
GustavoSwDaniel <gustavodanieldetoledo@gmail.com>
2023-06-18 01:22:30 -0300

    add in app

--- App.js
+++ App.js
@@ -4,12 +4,38 @@ import MapView from 'react-native-maps';
 import * as Location from 'expo-location'
 import * as Permissions from 'expo-permissions'
 import React, { useState, useEffect, useRef } from 'react';
-import Mapa from './src/components/Map';
 
 export default function App() {
+  
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status === 'granted') {
+        let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+        setOrigin({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        });
+        console.log(location);
+      } else {
+        throw new Error('Location permission not granted');
+      }
+    })();
+  }, []);
   return (
     <View style={styles.container}>
-      <Mapa/>
+      <MapView
+        style={styles.map}
+        initialRegion={origin}
+        showsUserLocation={true}
+        zoomEnabled={false}
+        loadingEnabled={true}
+      />
       <StatusBar style="auto" />
     </View>
   );
